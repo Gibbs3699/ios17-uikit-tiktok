3f59373f4fd1c0f1e2136d4454b44d0c480455c3
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-11-12 14:39:15 +0700

    init project

---
+++ iOS17 TikTok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS17 TikTok
+//
+//  Created by TheGIZzz on 12/11/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
